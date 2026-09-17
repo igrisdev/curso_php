a8510beffda05430b2f036e8a386b470078a6ce3
igrisdev <johanalvarezz237@gmail.com>
2026-09-17 09:42:03 -0500

    Add Database classes for Usuarios and Productos namespaces with connection methods

--- namespace.php
+++ namespace.php
@@ -1,3 +1,15 @@
 <?php
 // Namespaces
 namespace Usuarios;
+class Database {
+    public function conectar() {
+        echo "Conectando a la base de datos usuarios";
+    }
+}
+
+namespace Productos;
+class Database {
+    public function conectar() {
+        echo "Conectando a la base de datos productos";
+    }
+}
